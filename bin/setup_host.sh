9a3b0f2dfc91577ec65b990d7b4eae6856f5e926
dengl <guolong613@gmail.com>
2023-02-04 09:20:54 +0000

    创建production key

--- bin/setup_host.sh
+++ bin/setup_host.sh
@@ -10,7 +10,7 @@ if [ "$(docker ps -aq -f name=^mangosteen-prod-1$)" ]; then
   docker rm -f $container_name
 fi
 echo 'docker run ...'
-docker run -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name leaf:$version
+docker run -e DB_HOST=$DB_HOST -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name leaf:$version
 echo 'docker exec ...'
 docker exec -it $container_name bin/rails db:create db:migrate
 echo 'DONE!'
