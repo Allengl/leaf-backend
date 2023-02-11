cd3fcdfef6caebc757b106a5a9b594907e8f2130
dengl <guolong613@gmail.com>
2023-02-11 09:05:47 +0000

    更新 api 文档

--- spec/acceptance/items_spec.rb
+++ spec/acceptance/items_spec.rb
@@ -27,31 +27,4 @@ resource "账目" do
       expect(json['resources'].size).to eq 10
     end
   end
-
-  post "/api/v1/items" do
-    authentication :basic, :auth
-    parameter :amount, '金额（单位：分）', required: true
-    parameter :kind, '类型', required: true, enum: ['expenses', 'income']
-    parameter :happen_at, '发生时间', required: true
-    parameter :tags_id, '标签列表（只传ID）', required: true
-    with_options :scope => :resource do
-      response_field :id
-      response_field :amount
-      response_field :kind
-      response_field :happen_at
-      response_field :tags_id
-    end
-    let(:amount) { 9900 }
-    let(:kind) { 'expenses' }
-    let(:happen_at) { '2020-10-30T00:00:00+08:00' }
-    let(:tags) { (0..1).map{Tag.create name: 'x', sign:'x', user_id: current_user.id} }
-    let(:tags_id) { tags.map(&:id) }
-    let(:happen_at) { '2020-10-30T00:00:00+08:00' }
-    example "创建账目" do
-      do_request
-      expect(status).to eq 200
-      json = JSON.parse response_body
-      expect(json['resource']['amount']).to eq amount
-    end
-  end
 end
