cf13af679da1b197271be376196ecaf942298f97
dengl <guolong613@gmail.com>
2023-02-11 03:59:37 +0000

    给 user 添加了 jwt 相关的 2 个方法

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,5 @@
 require "rspec_api_documentation"
 
-module RequestTestHelper
-  def sign_in(user)
-    post "/api/v1/session", params: { email: user.email, code: "123456" }
-    json = JSON.parse response.body
-    { Authorization: "Bearer #{json["jwt"]}" }
-  end
-end
-
 RspecApiDocumentation.configure do |config|
   config.request_body_formatter = :json
 end
