9c3c63c4c21c40a7830fb267ae8b6f0194ec5b5b
dengl <guolong613@gmail.com>
2023-02-11 07:12:13 +0000

    创建 tags 表 和 tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_04_062938) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_11_070949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_062938) do
     t.datetime "happen_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "sign"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_062938) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tags", "users"
 end
