1a9dbd46665911c332f989cdb8ca569f82cddff5
dengl <guolong613@gmail.com>
2023-02-15 12:52:27 +0000

    修复所有测试用例

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,18 @@
 class Item < ApplicationRecord
   enum kind: { expenses: 1, income: 2 }
-  validates :kind, presence: true
   validates :amount, presence: true
+  validates :kind, presence: true
+  validates :happen_at, presence: true
   validates :tag_ids, presence: true
 
+  belongs_to :user
 
   validate :check_tag_ids_belong_to_user
-
-  belongs_to :user
 
   def check_tag_ids_belong_to_user
     all_tag_ids = Tag.where(user_id: self.user_id).map(&:id)
     if self.tag_ids & all_tag_ids != self.tag_ids
-      self.errors.add :tag_ids, '不属于当前用户'
+      self.errors.add :tag_ids, "不属于当前用户"
     end
   end
-
 end
