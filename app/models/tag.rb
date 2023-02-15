409a72e1bc8cf49b74d736776c8877a7b879cdc0
dengl <guolong613@gmail.com>
2023-02-15 15:29:52 +0000

    校验 tag name 不能超过 4 个字符

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,9 @@
 class Tag < ApplicationRecord
   paginates_per 25
   enum kind: { expenses: 1, income: 2 }
+  validates :name, length: { maximum: 4 }
   validates :name, presence: true
   validates :sign, presence: true
+  validates :kind, presence: true
   belongs_to :user
 end
